1c601131000c4bf1640d164e26620f65ee31bec8
Yash Kadam <kadamyash6112@gmail.com>
2025-01-31 22:33:01 +0530

    getting an FAQ based on languange

--- faq/serializers.py
+++ faq/serializers.py
@@ -3,6 +3,12 @@ from .models import FAQ
 
 
 class FAQSerializer(serializers.ModelSerializer):
+    translated_question = serializers.SerializerMethodField()
+
     class Meta:
         model = FAQ
-        fields = '__all__'
+        fields = ['id', 'translated_question', 'answer']
+
+    def get_translated_question(self, obj):
+        lang_code = self.context['request'].query_params.get('lang', 'en')
+        return obj.get_translated_question(lang_code)
--- faq/views.py
+++ faq/views.py
@@ -9,5 +9,14 @@ class FAQViewSet(viewsets.ModelViewSet):
     serializer_class = FAQSerializer
 
 
+def get_queryset(self):
+    lang_code = self.request.query_params.get('lang', 'en')
+    faqs = self.queryset
+    if lang_code in ['hi', 'bn']:
+        for faq in faqs:
+            faq.question = faq.get_translated_question(lang_code)
+    return faqs
+
+
 def home(request):
     return HttpResponse("Welcome to the FAQ API. Visit /admin or /api/faqs.")
